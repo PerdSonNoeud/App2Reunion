344e3123fd6a5662f3e54cf4ac8b1096cc1141d2
matheo <matheo.piget5@gmail.com>
2025-03-19 14:03:36 +0100

    connexion base de donnée correct

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'reunion',
+    user: process.env.DB_USER || 'admin',
+    password: process.env.DB_PASSWORD || 'admin',
+    port: process.env.DB_PORT || 5433,
+});
+
+pool.connect((err) => {
+    if (err) {
+        console.error('Erreur de connexion à la base de données', err.stack);
+    } else {
+        console.log('Connecté à la base de données');
+    }
+});
+
+module.exports = { pool };
--- sql/init.sql
+++ sql/init.sql
@@ -2,14 +2,19 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE meetings{
+CREATE TABLE meetings (
     id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description TEXT,
     start_time TIMESTAMP NOT NULL,
     end_time TIMESTAMP NOT NULL,
     user_id INT REFERENCES users(id)
-}
+);
+
+CREATE USER admin WITH PASSWORD 'admin';
+GRANT ALL PRIVILEGES ON DATABASE reunion TO admin;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO admin;
