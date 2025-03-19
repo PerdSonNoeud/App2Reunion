1a1cc1df1d21a4eed7cef7f1733e144d3e1262e6
matheo <matheo.piget5@gmail.com>
2025-03-19 12:19:54 +0100

    début sql

--- routes/new_meeting.js
+++ routes/new_meeting.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-// Correction: '/' au lieu de '/new_meeting'
 router.get('/', (req, res) => {
   res.render('meetings/new', { title: 'Nouvelle réunion', user: null });
 });
 
-// Correction: '/' au lieu de '/new_meeting'
 router.post('/', (req, res) => {
   res.send('Traitement de la création de la réunion');
 });
