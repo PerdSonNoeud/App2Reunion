73d2e639ad2a6112bf921c838fa596309c989cf7
Christophe <christophejin2004@gmail.com>
2025-03-24 16:37:57 +0100

    Fix de l'affichage de la connexion sur la page d'accueil.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('pages/index', { title: 'Accueil', user: null });
+  res.render('pages/index', { title: 'Accueil', user: req.session.user });
 });
 
 module.exports = router;
